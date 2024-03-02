064cd34154353805c600de998304dff88ff659dd
ettolrach <ettolrach@disroot.org>
2024-03-02 18:01:04 +0000

    Change UUID to v5

--- backend/src/llm.rs
+++ backend/src/llm.rs
@@ -1,11 +1,15 @@
 use crate::kube::Kube;use async_trait::async_trait;
 
+/// Trait for interacting with an LLM.
 #[async_trait]
 pub trait LLM {
+    /// Send a query to the LLM and get a [`std::string::String`] response.
     async fn query(input: &str) -> String;
+    /// Ask the LLM to combine the given Kubes and return a new Kube.
     async fn combine(&self, kubes: &[Kube]) -> Kube;
 }
 
+/// A fake LLM that functions very basically, not processing the input in any meaningful way. This is most useful for testing functionality of other features which use LLMs.
 pub struct FakeLLM {
 }
 impl FakeLLM {
@@ -24,7 +28,7 @@ impl LLM for FakeLLM {
         for kube in kubes {
             new_string.push_str(kube.name.as_str());
         }
-        let uuid = uuid::Uuid::new_v4();
+        let uuid = uuid::Uuid::new_v5(&uuid::Uuid::NAMESPACE_DNS, b"poo");
         Kube { name: new_string, uuid }		
     }
 }
@@ -35,8 +39,8 @@ mod tests {
     #[tokio::test]
     async fn fake_combine_test() {
         let kubes = vec![
-            Kube { name: String::from("water"), uuid: uuid::Uuid::new_v4() },
-            Kube { name: String::from("glass"), uuid: uuid::Uuid::new_v4() }
+            Kube { name: String::from("water"), uuid: uuid::Uuid::new_v5(&uuid::Uuid::NAMESPACE_DNS, b"poo") },
+            Kube { name: String::from("glass"), uuid: uuid::Uuid::new_v5(&uuid::Uuid::NAMESPACE_DNS, b"poo") }
         ];
         let fake_llm = FakeLLM::new();
         let response_kube = fake_llm.combine(&kubes).await;
