9eb489f9b00965d9a6655aeda8ae53bd76e1cc7f
Danny Piper <djpiper28@gmail.com>
2024-03-02 20:28:46 +0000

    feat; added getkube

--- kube_cache/model/model.go
+++ kube_cache/model/model.go
@@ -8,34 +8,41 @@ import (
 )
 
 type Kube struct {
-  Name string 
-  Id string `gorm:"primaryKey"` 
+	Name  string `json:"name"`
+	Id    string `gorm:"primaryKey" json:"id"`
+	Image []byte `json:"image"`
 }
 
 type KubeRecipe struct {
-  Id string `gorm:"primaryKey"`
-  Output Kube
-  Ingredients []Kube `gorm:"index"`
+	Id          string `gorm:"primaryKey" json:"id"`
+	Output      Kube   `gorm:"foreignKey:Id;references:Id" json:"output"`
+	Ingredients []Kube `gorm:"index" json:"ingredients"`
 }
 
 type Database struct {
-  Db *gorm.DB
+	Db *gorm.DB
 }
 
 func New(url string) *Database {
-  log.Println("Connecting to database...")
-  db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
-  if err != nil {
-    log.Fatal(err)
-  }
+	log.Println("Connecting to database...")
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  log.Println("Database connection established")
-  log.Println("Migrating database...")
-  err = db.AutoMigrate(&Kube{}, &KubeRecipe{})
-  if err != nil {
-    log.Fatal(err)
-  }
+	log.Println("Database connection established")
+	log.Println("Migrating database...")
+	err = db.AutoMigrate(&Kube{}, &KubeRecipe{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  log.Println("Migrated database successfully")
-  return &Database{Db: db}
+	log.Println("Migrated database successfully")
+	return &Database{Db: db}
+}
+
+func (db *Database) GetKube(id string) (Kube, error) {
+	var kube Kube
+	result := db.Db.First(&kube, "id = ?", id)
+	return kube, result.Error
 }
