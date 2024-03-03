b0d1fcc63edd44efb8fc9427d3c7617e8791410b
Danny Piper <djpiper28@gmail.com>
2024-03-03 08:22:31 +0000

    feat: added metrics exporter

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -1,9 +1,15 @@
 use crate::{ws, Clients, Result};
 use warp::Reply;
 
-pub async fn ws_handler(ws: warp::ws::Ws, clients: Clients) -> Result<impl Reply>
-{
+pub async fn ws_handler(ws: warp::ws::Ws, clients: Clients) -> Result<impl Reply> {
     println!("ws_handler"); //debug
 
     Ok(ws.on_upgrade(move |socket| ws::client_connection(socket, clients)))
+}
+
+pub async fn metrics_handler(clients: Clients) -> Result<impl Reply> {
+    let metricsString = "cosmic_kube_clients{type=\"connected\"} ".to_string()
+        + &clients.lock().await.len().to_string()
+        + "\n";
+    Ok(metricsString)
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -18,22 +18,23 @@ type Result<T> = std::result::Result<T, Rejection>;
 
 #[tokio::main]
 async fn main() {
-
     //initialise a hashmap to store currently connected clients. We may want some more logic here if we want currently connected clients to be stored somewhere
     let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
 
     println!("Configuring websocket route"); //debug
     let ws_route = warp::path("ws")
-    .and(warp::ws())
-    .and(with_clients(clients.clone()))
-    .and_then(handlers::ws_handler);
+        .and(warp::ws())
+        .and(with_clients(clients.clone()))
+        .and_then(handlers::ws_handler)
+        .or(warp::path("metrics")
+            .and(with_clients(clients.clone()))
+            .and_then(handlers::metrics_handler));
 
     let routes = ws_route.with(warp::cors().allow_any_origin());
     println!("Starting server on http://0.0.0.0:8000"); //debug
-    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await; 
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
 
 fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
     warp::any().map(move || clients.clone())
 }
-
