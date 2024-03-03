d64b6dd5d560685feb52c70d7ebde2f894e8be9c
Danny Piper <djpiper28@gmail.com>
2024-03-03 01:47:47 +0000

    feat: added new recipe

--- kube_cache/model/model.go
+++ kube_cache/model/model.go
@@ -19,12 +19,16 @@ type KubeRecipe struct {
 	Output      uuid.UUID         `json:"outputId" gorm:"index:idx_output_id"`
 	OutputKube  *Kube             `json:"outputKube" gorm:"foreignKey:Output;references:Id"`
 	Ingredients []KubeRecipeLines `json:"ingredients"`
+
+	Kube1Id string `json:"kube1_id" gorm:"index:idx_kube1_id"`
+	Kube1   *Kube  `json:"kube1" gorm:"foreignKey:Kube1Id;references:Id"`
+
+	Kube2Id string `json:"kube2_id" gorm:"index:idx_kube2_id"`
+	Kube2   *Kube  `json:"kube2" gorm:"foreignKey:Kube2Id;references:Id"`
 }
 
 type KubeRecipeLines struct {
 	KubeRecipeId uuid.UUID `json:"kube_recipe_id" gorm:"index:idx_kube_recipe_id"`
-	KubeId       string    `json:"kube_id" gorm:"index:idx_kube_id"`
-	Kube         *Kube     `json:"kube" gorm:"foreignKey:KubeId;references:Id"`
 }
 
 type Database struct {
@@ -57,18 +61,40 @@ func (db *Database) GetKube(id string) (Kube, error) {
 	return kube, result.Error
 }
 
-func (db *Database) SetKube(kube Kube) error {
-	result := db.Db.Create(&kube)
-	return result.Error
-}
-
-func (db *Database) GetKubeRecipe(id string) (KubeRecipe, error) {
+func (db *Database) GetKubeRecipe(kube1, kube2 string) (KubeRecipe, error) {
 	var recipe KubeRecipe
-	result := db.Db.Preload("OutputKube").Preload("Ingredients.Kube").First(&recipe, "id = ?", id)
+	result := db.Db.First(&recipe, "kube1 = ? AND kube2 = ?", kube1)
 	return recipe, result.Error
 }
 
-func (db *Database) SetKubeRecipe(recipe KubeRecipe) error {
-	result := db.Db.Create(&recipe)
-	return result.Error
+func (db *Database) SetKubeRecipe(kube1, kube2 Kube, newKube string) error {
+  log.Printf("Setting kube recipe: %s + %s = %s", kube1.Name, kube2.Name, newKube)
+  err := db.Db.Transaction(func(tx *gorm.DB) error {
+    newKubeObject := Kube{Name: newKube, Id: uuid.New()}
+    err := tx.Create(&newKubeObject).Error
+    if err != nil {
+      log.Printf("Cannot create new kube: %s", err)
+      return err
+    }
+
+    recipe := KubeRecipe{
+      Id:     uuid.New(),
+      Output: newKubeObject.Id,
+      Kube1:  &kube1,
+      Kube2:  &kube2,
+    }
+    err = tx.Create(&recipe).Error
+    if err != nil {
+      log.Printf("Cannot create new kube recipe: %s", err)
+      return err
+    }
+    tx.Commit()
+    return nil
+  })
+
+  if err != nil {
+    log.Print("Saving the kube recipe failed")
+    return err
+  }
+  return nil
 }
--- kube_cache/server/cache.go
+++ kube_cache/server/cache.go
@@ -1,19 +1,81 @@
 package server
 
 import (
+	"log"
+
 	"github.com/CosmicKube/kube_cache/aiStuff"
 	"github.com/CosmicKube/kube_cache/model"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type Server struct {
 	Database *model.Database
-  Ai *aiStuff.KubeAi
+	Ai       *aiStuff.KubeAi
 }
 
 func New(database *model.Database, ai *aiStuff.KubeAi) *Server {
-  return &Server{Database: database, Ai: ai}
+	return &Server{Database: database, Ai: ai}
 }
 
 func (s *Server) Use(engine *gin.Engine) {
+	engine.GET("/kube/:id", s.GetKube)
+	engine.GET("/kuberecipe/:id1/:id2", s.GetKubeRecipe)
+}
+
+func (s *Server) GetKube(c *gin.Context) {
+	id := c.Param("id")
+	kube, err := s.Database.GetKube(id)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, kube)
+}
+
+// 24 hours
+const cacheControlHeader = "max-age=86400"
+
+func (s *Server) GetKubeRecipe(c *gin.Context) {
+	id1 := c.Param("id1")
+	id2 := c.Param("id2")
+	recipe, err := s.Database.GetKubeRecipe(id1, id2)
+	if err != nil {
+		kube1, err := s.Database.GetKube(id1)
+		if err != nil {
+			log.Printf("Cannot get kube: %s", err)
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		kube2, err := s.Database.GetKube(id2)
+		if err != nil {
+			log.Printf("Cannot get kube: %s", err)
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		newKube, err := s.Ai.GenerateKubeRecipe(kube1.Name, kube2.Name)
+		if err != nil {
+			log.Printf("Cannot generate kube recipe: %s", err)
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		err = s.Database.SetKubeRecipe(kube1, kube2, newKube)
+		if err != nil {
+			log.Printf("Cannot save kube recipe: %s", err)
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		recipe, err = s.Database.GetKubeRecipe(id1, id2)
+		if err != nil {
+			log.Printf("Cannot get kube recipe: %s", err)
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+	}
+  c.Header("Cache-Control", cacheControlHeader)
+	c.JSON(200, recipe)
 }
