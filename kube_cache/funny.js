6874dca9d7c821146da4a2457cf7e300c779996c
Danny Piper <djpiper28@gmail.com>
2024-03-03 11:28:10 +0000

    feat: AJIOAShdaskjdhaskjldhsajkdhasjd

--- kube_cache/funny.js
+++ kube_cache/funny.js
@@ -5,16 +5,20 @@ fetch(kubes)
   .then(async (data) => {
     for (let i = 0; i < data.length; i++) {
       for (let j = 0; j < i; j++) {
-        await new Promise((resolve) => setTimeout(resolve, 10000));
         const url =
           "https://hack.djpiper28.co.uk/cache/kubeRecipeByIds/" +
           data[i].id +
           "/" +
           data[j].id;
         console.log(url);
-        await fetch(url)
+        await fetch(url, {
+          timeout: 1000,
+        })
           .then((res) => console.log(res.json))
-          .catch((err) => console.error(err));
+          .catch(async (err) => {
+            console.error(err);
+            await new Promise((resolve) => setTimeout(resolve, 10000));
+          });
       }
     }
   });
