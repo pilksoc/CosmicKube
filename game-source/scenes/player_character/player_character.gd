bc8910119a930742925164186b77db4231f2d5fd
charlie.aylott <caylott@outlook.com>
2024-03-03 04:18:26 +0000

    task: newlined were removed in this commit

--- game-source/scenes/player_character/player_character.gd
+++ game-source/scenes/player_character/player_character.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 
 @export var tile_size: float = 100
 @export var is_moving: bool = false
-@export var animation_speed: float = 4
 
 var inputs = {
 	"right": Vector2.RIGHT,
@@ -12,16 +11,12 @@ var inputs = {
 	"down": Vector2.DOWN
 }
 
-
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size / 2
-	
-
 
 func _input(event):
-	var vec = Input.get_vector("left","right","up","down")
-	 
+	var vec = Input.get_vector("left", "right", "up", "down")
 	if !is_moving and vec.length()==1:
 		position += vec*100
 	
