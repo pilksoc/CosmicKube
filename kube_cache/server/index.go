cff25712d37d6cb4b593fb16495e2023150efeb0
Danny Piper <djpiper28@gmail.com>
2024-03-04 18:56:42 +0000

    fix: align flexbox in cache control

--- kube_cache/server/index.go
+++ kube_cache/server/index.go
@@ -18,8 +18,8 @@ func (s *Server) Index(c *gin.Context) {
 		kubesHtml += fmt.Sprintf(`<div style="max-width: 100px;">
       <h3>%s</h3>
       <a href="./kubeImageByIdNew/%s">Regenerate Image</a>
-      <img src="./kubeImageById/%s" style="max-width: 100px; max-height: 100px;"/>
-    </div>`, kube.Name, kube.Id, kube.Id)
+      <img src="./kubeImageById/%s" style="max-width: 100px; max-height: 100px; align-items: center; align-content: center;" loading="lazy" alt="%s"/>
+    </div>`, kube.Name, kube.Id, kube.Id, kube.Name)
 	}
 
 	body := fmt.Sprintf(`<!DOCTYPE html>
@@ -30,11 +30,14 @@ func (s *Server) Index(c *gin.Context) {
   <h1>Kube Cache</h1>
   <h2>Endpoints</h2>  
   <ul>
+    <li><a href="./">/.</a></li>
     <li><a href="./kubes">/kubes</a></li>
     <li><a href="./kubeById/:id">/kubeById/:id</a></li>
     <li><a href="./kubeImageById/:id">/kubeImageById/:id</a></li>
     <li><a href="./kubeRecipes">/kubeRecipes</a></li>
     <li><a href="./kubeRecipeByIds/:id1/:id2">/kubeRecipeByIds/:id1/:id2</a></li>
+    <li><a href="./cache_metrics">/cache_metrics</a></li>
+    <li><a href="./metrics">/metrics</a></li>
   </ul>
   <h2>Kubes</h2>
   <div style="display: flex; flex-direction: row; flex-wrap: wrap; gap: 10px;">
