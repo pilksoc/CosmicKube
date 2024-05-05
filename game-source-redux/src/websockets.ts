27679e99972ea75d3f2603c1c32bb90ec8942310
Danny Piper <djpiper28@gmail.com>
2024-05-05 17:38:40 +0100

    feat: added callbacks for game events

--- game-source-redux/src/websockets.ts
+++ game-source-redux/src/websockets.ts
@@ -1,8 +1,37 @@
+import { v4 } from "uuid";
+import {
+  Coordinate,
+  Kube,
+  Player,
+  PlayerInfo,
+  SpaceKinds,
+} from "./types/kube_types";
+
 const baseUrl = "https://hack.djpiper28.co.uk/ws/";
 
+export const ERR_VAL = -9999999;
+
+/**
+ * This contains the change of position of the player
+ */
+export type PlayerCoordinateDelta = {
+  oldCoord: Coordinate;
+  newCoord: Coordinate;
+};
+
 class KubeWebSocket {
+  // Socket Shite
   public onError: (err: string) => void = () => {};
   public onClose: () => void = () => {};
+
+  // Game actions
+  public onPlayerAction: (
+    coords: PlayerCoordinateDelta,
+    player: Player,
+  ) => void;
+  public onKubeAction: (coords: PlayerCoordinateDelta, kube: Kube) => void;
+  public onEmptyAction: (coords: PlayerCoordinateDelta) => void;
+
   private ws: WebSocket;
 
   constructor() {
@@ -14,6 +43,47 @@ class KubeWebSocket {
 
   private onMessage(event: MessageEvent) {
     console.log(event.data);
+    const data: string = event.data;
+    const eventData: PlayerInfo = JSON.parse(data) as PlayerInfo;
+
+    const action = eventData.action;
+    const coordDelta: PlayerCoordinateDelta = {
+      oldCoord: eventData.old_coordinates ?? [ERR_VAL, ERR_VAL],
+      newCoord: eventData.coordinates,
+    };
+    if (action) {
+      switch (action.kind) {
+        case SpaceKinds.EMPTY:
+          this.onEmptyAction(coordDelta);
+          break;
+        case SpaceKinds.KUBE:
+          if (!action.kube) throw new Error("Shit is fucked");
+          this.onKubeAction(coordDelta, action.kube);
+          break;
+        case SpaceKinds.PLAYER:
+          if (!action.player) throw new Error("Shit is fucked");
+          this.onPlayerAction(coordDelta, action.player);
+          break;
+      }
+    }
+  }
+
+  public connectAs(name: string) {
+    const data: PlayerInfo = {
+      initialised: false,
+      player: {
+        uuid: v4(),
+        username: name,
+      },
+      coordinates: [0, 0],
+    };
+
+    this.send(JSON.stringify(data));
+  }
+
+  private send(data: string) {
+    console.log(`Sending: ${data}`);
+    this.ws.send(data);
   }
 }
 
